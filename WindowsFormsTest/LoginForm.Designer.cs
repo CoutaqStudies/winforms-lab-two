6f0d540998f0f0ec840bd1927e1169977c8165c5
Michael Melikov <coutaq@gmail.com>
2020-03-13 01:52:26 +0300

    Add project files.

---
+++ WindowsFormsTest/LoginForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsTest
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.showPassword = new System.Windows.Forms.CheckBox();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.textWelcome = new System.Windows.Forms.Label();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.labelPassword, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPassword, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLogin, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.showPassword, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.buttonLogIn, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.textWelcome, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelLogin, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 11;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.textBoxPassword.Location = new System.Drawing.Point(266, 240);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(266, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.Text = "Password";
+            this.textBoxPassword.Click += new System.EventHandler(this.textBoxPassword_Clicked);
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxLogin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLogin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLogin.Location = new System.Drawing.Point(266, 160);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(266, 20);
+            this.textBoxLogin.TabIndex = 0;
+            this.textBoxLogin.Text = "Login";
+            this.textBoxLogin.Click += new System.EventHandler(this.textBoxLogin_Clicked);
+            this.textBoxLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxLogin_Enter);
+            // 
+            // showPassword
+            // 
+            this.showPassword.AutoSize = true;
+            this.showPassword.Dock = System.Windows.Forms.DockStyle.Top;
+            this.showPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.showPassword.Font = new System.Drawing.Font("Gotham Narrow Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.showPassword.Location = new System.Drawing.Point(266, 280);
+            this.showPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.showPassword.Name = "showPassword";
+            this.showPassword.Size = new System.Drawing.Size(266, 23);
+            this.showPassword.TabIndex = 3;
+            this.showPassword.Text = "Show password";
+            this.showPassword.UseVisualStyleBackColor = true;
+            this.showPassword.CheckedChanged += new System.EventHandler(this.showPassword_CheckedChanged);
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLogIn.FlatAppearance.BorderSize = 0;
+            this.buttonLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogIn.Font = new System.Drawing.Font("Gotham Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonLogIn.Location = new System.Drawing.Point(266, 320);
+            this.buttonLogIn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(266, 40);
+            this.buttonLogIn.TabIndex = 4;
+            this.buttonLogIn.Text = "Login";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Visible = false;
+            // 
+            // textWelcome
+            // 
+            this.textWelcome.AutoSize = true;
+            this.textWelcome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textWelcome.Font = new System.Drawing.Font("Gotham Medium", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textWelcome.ForeColor = System.Drawing.Color.Black;
+            this.textWelcome.Location = new System.Drawing.Point(266, 80);
+            this.textWelcome.Margin = new System.Windows.Forms.Padding(0);
+            this.textWelcome.Name = "textWelcome";
+            this.textWelcome.Size = new System.Drawing.Size(266, 40);
+            this.textWelcome.TabIndex = 5;
+            this.textWelcome.Text = "Welcome.";
+            this.textWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelLogin.Font = new System.Drawing.Font("Gotham Medium", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.labelLogin.Location = new System.Drawing.Point(266, 120);
+            this.labelLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(54, 40);
+            this.labelLogin.TabIndex = 6;
+            this.labelLogin.Text = "Login:";
+            this.labelLogin.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelPassword.Font = new System.Drawing.Font("Gotham Medium", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.labelPassword.Location = new System.Drawing.Point(266, 200);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(86, 40);
+            this.labelPassword.TabIndex = 7;
+            this.labelPassword.Text = "Password:";
+            this.labelPassword.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.labelPassword.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.CheckBox showPassword;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.Label textWelcome;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+    }
+}
+
---
+++ WindowsFormsTest/LoginForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTest
+{
+    public partial class LoginForm : System.Windows.Forms.Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBoxLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            buttonLogIn.Visible = true;
+        }
+        private void textBoxLogin_Enter(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+        if (e.KeyChar == (char)Keys.Return)
+            {
+                textBoxPassword.Select();
+            }
+        }
+        private void textBoxPassword_Clicked(object sender, EventArgs e)
+        {
+            if (textBoxPassword.Text == "Password")
+                textBoxPassword.ResetText();
+            if (showPassword.Checked)
+                textBoxPassword.PasswordChar = '\0';
+            else
+                textBoxPassword.PasswordChar = '*';
+        }
+        private void textBoxLogin_Clicked(object sender, EventArgs e)
+        {
+            textBoxLogin.Text = String.Empty;
+        }
+
+        private void showPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (showPassword.Checked)
+                textBoxPassword.PasswordChar = '\0';
+            else
+                textBoxPassword.PasswordChar = '*';
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
